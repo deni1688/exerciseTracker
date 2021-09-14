ebf58e01aff4b6ce07301abbf22bf92b1f2747e8
Denis Junuzovic <denisj1688@gmail.com>
2021-09-14 13:07:00 +0200

    dclarative api

---
+++ api.go
@@ -0,0 +1,43 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type api struct {
+	Engine *gin.Engine
+}
+
+func newApi(engine *gin.Engine) *api {
+	return &api{engine}
+}
+
+func (a *api) InitExerciseApi() *api {
+	a.Engine.GET("/exercises", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "noImplemented",
+		})
+	})
+
+	a.Engine.POST("/exercises", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "noImplemented",
+		})
+	})
+
+	return a
+}
+
+func (a *api) InitWeightApi() *api {
+	a.Engine.GET("/weight", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "noImplemented",
+		})
+	})
+
+	a.Engine.POST("/weight", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "noImplemented",
+		})
+	})
+
+	return a
+}
--- main.go
+++ main.go
@@ -5,17 +5,8 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/exercises", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "notImplemented",
-		})
-	})
-
-	r.POST("/exercises", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "notImplemented",
-		})
-	})
-
-	r.Run()
+	newApi(r).
+		InitExerciseApi().
+		InitWeightApi().
+		Engine.Run()
 }
