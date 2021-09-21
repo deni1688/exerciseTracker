9fd53a6bd638b7d12cc1a3d191113a174ae14699
Denis Junuzovic <denisj1688@gmail.com>
2021-09-21 09:45:31 +0200

    revert aggregate

--- tracker/aggregate.go
+++ tracker/aggregate.go
@@ -1,7 +1,11 @@
 package tracker
 
 type ExerciseAggregate struct {
-	*exercise
-	*cardio
-	*calisthenics
+	Category string  `json:"category"`
+	Name     string  `json:"name"`
+	Weight   float64 `json:"weight"`
+	Duration int     `json:"duration,omitempty"`
+	Distance int     `json:"distance,omitempty"`
+	Reps     int     `json:"reps,omitempty"`
+	Sets     int     `json:"sets,omitempty"`
 }
