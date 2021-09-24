6c3983542ea11cc783ac384bbd9798f5a7e70510
Denis Junuzovic <denisj1688@gmail.com>
2021-09-24 18:17:32 +0200

    create cli command

--- cli/create.go
+++ cli/create.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"deni1688/exercise_tracker/domain"
 	"fmt"
+	"strconv"
+
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,22 @@ func getCreateCmd(service domain.Service) *cobra.Command {
 		Short: "creates a new exercise from args",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ex := new(domain.Exercise)
+
+			category := args[0]
+			name := args[1]
+			weight, _ := strconv.ParseFloat(args[2], 64)
+			duration, _ := strconv.Atoi(args[2])
+			distance, _ := strconv.Atoi(args[2])
+			reps, _ := strconv.Atoi(args[2])
+			sets, _ := strconv.Atoi(args[2])
+
+			ex.Category = category
+			ex.Name = name
+			ex.Weight = weight
+			ex.Duration = duration
+			ex.Distance = distance
+			ex.Reps = reps
+			ex.Sets = sets
 
 			id, err := service.SaveExercise(ex)
 			if err != nil {
